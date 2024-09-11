eff712859be3e69049a55c1d754504a2570f578f
lisasivak <30319803+lisasivak@users.noreply.github.com>
2024-09-11 13:46:12 +0200

    upd score.r

--- data/score.R
+++ data/score.R
@@ -7,26 +7,31 @@ score <- function(predictions_df, ground_truth_df) {
                     all.y = TRUE) # right join
   
   # Calculate accuracy
-  accuracy = sum( merged_df$prediction == merged_df$new_child) / length(merged_df$new_child) 
+  accuracy = sum( merged_df$prediction == merged_df$new_child, na.rm = T) / length(merged_df$new_child) 
   
-  # Calculate true positives, false positives, and false negatives
-  true_positives = sum( merged_df$prediction == 1 & merged_df$new_child == 1 )
-    
-  false_positives = sum( merged_df$prediction == 1 & merged_df$new_child == 0 )
-    
-  false_negatives = sum( merged_df$prediction == 0 & merged_df$new_child == 1 )
-    
+  # Calculate true positives and false positives 
+  true_positives = sum( merged_df$prediction == 1 & merged_df$new_child == 1, na.rm = T )
+  
+  false_positives = sum( merged_df$prediction == 1 & merged_df$new_child == 0 , na.rm = T)
+  
+  #false_negatives = sum( merged_df$prediction == 0 & merged_df$new_child == 1 , na.rm = T)
+  
+  # Calculate the actual number of positive instances (N of people who actually had a new child) for calculating recall
+  n_all_positive_instances = sum(ground_truth_df$new_child == 1)
+  
   # Calculate precision, recall, and F1 score
   if( (true_positives + false_positives) == 0) {
     precision = 0
   } else {
     precision = true_positives / (true_positives + false_positives)
   }
-  if( (true_positives + false_negatives) == 0) {
-    recall = 0
-  } else {
-    recall = true_positives / (true_positives + false_negatives)
-  }
+  # if( (true_positives + false_negatives) == 0) {
+  #   recall = 0
+  # } else {
+  #   recall = true_positives / (true_positives + false_negatives)
+  # }
+  recall = true_positives / n_all_positive_instances
+  
   if( (precision + recall) == 0) {
     f1_score = 0
   } else {
@@ -41,5 +46,5 @@ score <- function(predictions_df, ground_truth_df) {
   )
   
   return(metrics_df)
-
+  
 }
